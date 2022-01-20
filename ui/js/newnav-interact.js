f36d614f4490d36d182583f0972b2ddd605888a6
Bryan Carney <bryancarney@Bryans-MacBook-Air.local>
2022-01-20 11:54:54 -0800

    starpoing typo js

--- ui/js/newnav-interact.js
+++ ui/js/newnav-interact.js
@@ -8,7 +8,7 @@ jQuery( document ).ready( function( $ ) {
 			    $(window).scrollTop($(".new-site-nav__search-dropdown").scrollTop() );
 			  	} else {
 				  	$('.new-nav__burger .fas').removeClass('fa-times').addClass('fa-bars');
-				if (typeof startpoint !== "undefined" || startpoing !== null) {				
+				if (typeof startpoint !== "undefined" || startpoint !== null) {				
 					$(window).scrollTop(window.startPoint);
 				}
 				}
